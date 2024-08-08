ac3f5f619c30f0428ffcf2924b375652fd834e16
I`m Taerae <105408841+TaeRaeKim@users.noreply.github.com>
2024-08-08 10:55:16 +0900

    Create ColorConsole.js

---
+++ ColorConsole.js
@@ -0,0 +1,41 @@
+const Text_Color = {
+    "Black": 30,
+    "Red": 31,
+    "Green": 32,
+    "Yellow": 33,
+    "Blue": 34,
+    "Magenta": 35,
+    "Cyan": 36,
+    "White": 37,
+}
+
+const Bg_Color = {
+    "Black": 40,
+    "Red": 41,
+    "Green": 42,
+    "Yellow": 43,
+    "Blue": 44,
+    "Magenta": 45,
+    "Cyan": 46,
+    "White": 47
+}
+
+function print(text, color){
+    console.log(`\x1b[${color}m ${text} \x1b[0m`);
+}
+function colorText(text, color){
+    return `\x1b[${color}m ${text} \x1b[0m`;
+}
+
+module.exports = {
+    Text_Color,
+    Bg_Color,
+    print,
+    colorText
+}
+
+/*
+example
+print("부스트캠프 웹·모바일 9기 챌린지", Text_Color.Blue);
+console.log(colorText("J070 김태래", Text_Color.Cyan))
+*/
